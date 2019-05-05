02fc954faaa1aa10f654f70c6e7da1100934a44a
jyang001 <joeyang095@gmail.com>
2019-05-05 00:36:55 -0400

    remove calling API from MainActivity

--- app/src/main/java/com/example/guardiannews/MainActivity.java
+++ app/src/main/java/com/example/guardiannews/MainActivity.java
@@ -13,17 +13,12 @@ public class MainActivity extends AppCompatActivity {
 
     private static final int LOADER_ID = 1;
 
-    /** Guardian API Key **/
-    final String GUARDIAN_NEWS_URL = "?api-key=c7771d54-6420-45bf-b2c9-75182b3f2479&show-fields=thumbnail";
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d("ONCREATE: ","STARTING APP" );
-
-        List<Article> articles = QueryUtils.getArticles(GUARDIAN_NEWS_URL);
-
+        
         android.support.v7.widget.Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
     }
